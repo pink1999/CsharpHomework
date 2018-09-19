6c1ceb0a740eca2d43809041913f87812d22aeb2
pink1999 <43169767+pink1999@users.noreply.github.com>
2018-09-19 19:09:04 +0800

    Add files via upload

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //从键盘上接受两个数字 
+
+            string s = "";
+            int a = 0;
+            int b = 0;
+            Console.Write("Please input an int : ");
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            Console.Write("Please input another int : ");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            //计算a*b             int c = a * b;
+            //显示a*b的结果             Console.WriteLine(a + " * " + b + " = " + c);
+        }
+    }
+}
