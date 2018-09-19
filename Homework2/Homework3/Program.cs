c4a1165db98b0111cf84baf5cb734772e9dc7813
pink1999 <43169767+pink1999@users.noreply.github.com>
2018-09-19 19:19:07 +0800

    Add files via upload

---
+++ Homework2/Homework3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2到100以内的素数有：");
+            int i, j = 2;
+            for (i = 2; i < 100; i++)
+            {
+                for (j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                        break;
+
+                }
+                if (j == i)
+                    Console.Write(i + " ");
+
+            }
+        }
+    }
+}
