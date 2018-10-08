8dcd79bb18ab4b0b39d38b5c278e8614974d30fc
pink1999 <43169767+pink1999@users.noreply.github.com>
2018-10-08 09:53:35 +0800

    Add files via upload

---
+++ Homework4/AlarmClock/Clock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    partial class Program
+    {
+        class Clock
+        {
+            private int hour, minute, second;
+            public Clock(int hour, int minute, int second)
+            {
+                this.hour = hour;
+                this.minute = minute;
+                this.second = second;
+            }
+            //开始计时
+            public void Begin()
+            {
+
+                Timer timer = new Timer
+                {
+                    Enabled = true,
+                    Interval = 1000
+
+                };
+                timer.Start();
+                timer.Elapsed += timer_Elapsed;
+                timer.AutoReset = true;
+
+
+            }
+            void timer_Elapsed(object source, ElapsedEventArgs e)
+            {
+                if (DateTime.Now.Hour == hour && DateTime.Now.Minute == minute && DateTime.Now.Second == second)
+                {
+                    Console.WriteLine("到" + DateTime.Now.ToString() + "了！！");
+                }
+            }
+
+        }
+    }
+    
+}
---
+++ Homework4/AlarmClock/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    partial class Program
+    {
+        //使用事件机制，模拟实现一个闹钟的定时功能，可以设置闹钟，在闹钟时间到了以后，在控制台显示提示信息
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.WriteLine("请输入闹钟的时：");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            if (hour<0||hour>23)
+            {
+                Console.WriteLine("错误的信息！");
+                
+            }
+            Console.WriteLine("请输入闹钟的分：");
+            int minute = Convert.ToInt32(Console.ReadLine());
+            if (minute < 0 || minute > 59)
+            {
+                Console.WriteLine("错误的信息！");
+
+            }
+            Console.WriteLine("请输入闹钟的秒：");
+            int second = Convert.ToInt32(Console.ReadLine());
+            if (second < 0 || second > 59)
+            {
+                Console.WriteLine("错误的信息！");
+
+            }
+            Clock MyClock = new Clock(hour,minute,second);
+            MyClock.Begin();
+            //等待键盘输入，退出程序。使调试时能看到输出结果。如果没有此句，命令窗口会一闪而过。 
+            Console.ReadKey();
+
+        }
+    }
+}
